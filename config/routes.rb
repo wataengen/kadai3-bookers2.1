e1f7f2c43d17e83e4e43dec559f4dc1402236fe2
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-07-10 13:46:36 +0000

    [Start]1からやり直し

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root to: 'homes#index'
+  devise_for :users
+  get 'homes/about' => 'homes#about'
+  resources :homes, only: [:index,:show]
+  resources :books, only: [:new, :edit, :show, :index]
+  resources :users, only:[:index, :show, :edit]
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
