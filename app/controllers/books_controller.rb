b236287847a07d179d10d5e9e5fd7a9c25ed77c5
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-07-13 20:54:28 +0000

    [Update]リンクの修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
     flash[:notice] = 'You have created book successfully.'
     redirect_to book_path(@book.id)
     else
+      @user = current_user
       render :edit
     end
   end
